4a24e31451832c00d60ac0f6fad43fe5dd3388f2
sachingupta67 <sachingupta769@outlook.com>
2021-08-06 00:54:59 +0530

    console for checking env

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("process::::::", process.env);
   return (
     <div className="App">
       <header className="App-header">
