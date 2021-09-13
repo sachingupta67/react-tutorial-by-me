42ec71591ffe1e8888793882970344da71249297
sachin81 <sachin@hubilo.com>
2021-09-13 13:18:18 +0530

    counter application added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Application from 'application';
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Application />
+    </div>
+  );
 };
 
 export default App;
